fa4da9c155c6ca5946e1b9ae8df0fdaf89f609e5
Photon Cat <lkstc112233@gmail.com>
2018-08-18 21:05:09 -0700

    Removed mouse controlled fan.

--- DotsDance/main.js
+++ DotsDance/main.js
@@ -5,39 +5,13 @@ define(["require", "exports", "./Character"], function (require, exports, Charac
     hello.innerText = 'Hello world!';
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext('2d');
-    var angle = 0;
-    var centerX = 200;
-    var centerY = 200;
-    var radius = 100;
-    var count = 1;
     var char1 = new Character_1.Character();
     var char2 = new Character_1.Character();
-    function drawLine(count) {
-        if (count === void 0) { count = 1; }
-        context.beginPath();
-        context.lineWidth = 10;
-        if (count > 30) {
-            context.lineWidth = 3;
-        }
-        var step = Math.PI * 2 / count;
-        for (var i = 0; i < count; ++i) {
-            context.moveTo(centerX, centerY);
-            context.lineTo(centerX + radius * Math.cos(angle + step * i), centerY + radius * Math.sin(angle + step * i));
-        }
-        context.stroke();
-    }
     function gameLoop() {
         requestAnimationFrame(gameLoop);
-        angle += Math.PI / 60;
         context.clearRect(0, 0, canvas.width, canvas.height);
-        drawLine(count);
         char1.draw(context);
     }
-    canvas.addEventListener("mousemove", function (ev) {
-        var rect = canvas.getBoundingClientRect();
-        centerX = ev.clientX - rect.left;
-        centerY = ev.clientY - rect.top;
-    });
     document.addEventListener('keydown', function (ev) {
         if (ev.keyCode == 65) { // 'A'
             char1.x -= 3;
@@ -51,7 +25,6 @@ define(["require", "exports", "./Character"], function (require, exports, Charac
         if (ev.keyCode == 83) { // 'S'
             char1.y += 3;
         }
-        console.log(ev.keyCode);
     });
     requestAnimationFrame(gameLoop);
 });
--- DotsDance/main.ts
+++ DotsDance/main.ts
@@ -7,43 +7,14 @@ hello.innerText = 'Hello world!';
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const context = canvas.getContext('2d')!;
 
-let angle = 0;
-let centerX = 200;
-let centerY = 200;
-const radius = 100;
-
-let count = 1;
-
 const char1 = new Character();
 const char2 = new Character();
 
-function drawLine(count:number = 1) {
-    context.beginPath();
-    context.lineWidth = 10;
-    if (count > 30) {
-        context.lineWidth = 3;
-    }
-    const step = Math.PI * 2 / count;
-    for (let i = 0; i < count; ++i) {
-        context.moveTo(centerX, centerY);
-        context.lineTo(centerX + radius * Math.cos(angle + step * i), centerY + radius * Math.sin(angle + step * i));
-    }
-    context.stroke();
-}
-
 function gameLoop() {
     requestAnimationFrame(gameLoop);
-    angle += Math.PI / 60;
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawLine(count);
     char1.draw(context);
 }
-
-canvas.addEventListener("mousemove",(ev) => {
-    var rect = canvas.getBoundingClientRect();
-    centerX = ev.clientX - rect.left;
-    centerY = ev.clientY - rect.top;
-});
 
 document.addEventListener('keydown', (ev)=>{
     if (ev.keyCode == 65) { // 'A'
@@ -58,7 +29,6 @@ document.addEventListener('keydown', (ev)=>{
     if (ev.keyCode == 83) { // 'S'
         char1.y += 3;
     }
-    console.log(ev.keyCode);
 });
 
 requestAnimationFrame(gameLoop);
