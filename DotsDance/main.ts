671452035e415893198141434ebbb73c8490999f
Photon Cat <lkstc112233@gmail.com>
2018-08-21 17:05:14 -0400

    Added mouse control

--- DotsDance/main.ts
+++ DotsDance/main.ts
@@ -14,9 +14,27 @@ const char2 = new Character();
 const acc = new Point();
 const vel = new Point();
 
+let magnetOn = false;
+let mouseX = 0;
+let mouseY = 0;
+
+function updateMouseAcc() {
+    var rect = canvas.getBoundingClientRect();
+    acc.x = mouseX - rect.left;
+    acc.y = mouseY - rect.top;
+    acc.minus(char1.position);
+    acc.normalize();
+    acc.mul(0.15);
+}
+
 function gameLoop() {
     requestAnimationFrame(gameLoop);
     context.clearRect(0, 0, canvas.width, canvas.height);
+    if (magnetOn) {
+        updateMouseAcc();
+    } else {
+        acc.zero();
+    }
     vel.plus(acc);
     vel.mul(0.95);
     char1.position.plus(vel);
@@ -24,6 +42,23 @@ function gameLoop() {
 }
 
 const defaultAcc = 0.1;
+
+canvas.addEventListener("mousemove",(ev) => {
+    mouseX = ev.clientX;
+    mouseY = ev.clientY;
+});
+
+canvas.addEventListener("mousedown", (ev) => {
+    if (ev.button == 0) {
+        magnetOn = true;
+    }
+});
+
+canvas.addEventListener("mouseup", (ev) => {
+    if (ev.button == 0) {
+        magnetOn = false;
+    }
+});
 
 document.addEventListener('keydown', (ev)=>{
     if (ev.keyCode == 65) {         // 'A'
