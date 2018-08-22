fbf5b183cce4db0febf7af4eba0dcfddaf676b28
Photon Cat <lkstc112233@gmail.com>
2018-08-22 15:39:57 -0400

    Replaced character with circle

--- Controller/Character.ts
+++ Controller/Character.ts
@@ -1,13 +1,14 @@
 import {Point} from './xyTuple'
+import {circle} from './DrawingHelper'
 
 export class Character {
     private frame = 0;
     position = new Point();
 
     draw(context: CanvasRenderingContext2D) {
-        const radius = 50;
+        const radius = 25;
         this.frame += Math.PI / 60;
         
-        context.fillRect(this.position.x - radius / 2, this.position.y - radius / 2, radius, radius);
+        circle(context, this.position.x, this.position.y, radius, '#0004');
     }
 }
