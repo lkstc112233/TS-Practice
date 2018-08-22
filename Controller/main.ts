2a693e04049a9ebc823d362fc2408fec37da1071
Photon Cat <lkstc112233@gmail.com>
2018-08-22 16:51:55 -0400

    Added controller control

--- Controller/main.ts
+++ Controller/main.ts
@@ -8,52 +8,41 @@ const context = canvas.getContext('2d')!;
 const char = new Character();
 const controller = new Controller();
 
-const acc = new Point();
 const vel = new Point();
-
-let magnetOn = false;
-let mouseX = 0;
-let mouseY = 0;
-
-function updateMouseAcc() {
-    var rect = canvas.getBoundingClientRect();
-    acc.x = mouseX - rect.left;
-    acc.y = mouseY - rect.top;
-    acc.minus(char.position);
-    acc.normalize();
-    acc.mul(0.15);
-}
 
 function gameLoop() {
     requestAnimationFrame(gameLoop);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    if (magnetOn) {
-        updateMouseAcc();
-    } else {
-        acc.zero();
-    }
+    const acc = controller.getControllerValue();
+    acc.mul(0.3);
     vel.plus(acc);
     vel.mul(0.97);
     char.position.plus(vel);
     char.draw(context);
+    controller.draw(context);
 }
 
 const defaultAcc = 0.1;
 
 canvas.addEventListener("mousemove",(ev) => {
-    mouseX = ev.clientX;
-    mouseY = ev.clientY;
+    var rect = canvas.getBoundingClientRect();
+    var x = ev.clientX - rect.left;
+    var y = ev.clientY - rect.top;
+    controller.touchUpdate(new Point(x, y));
 });
 
 canvas.addEventListener("mousedown", (ev) => {
     if (ev.button == 0) {
-        magnetOn = true;
+        var rect = canvas.getBoundingClientRect();
+        var x = ev.clientX - rect.left;
+        var y = ev.clientY - rect.top;
+        controller.touchBegin(new Point(x, y));
     }
 });
 
 canvas.addEventListener("mouseup", (ev) => {
     if (ev.button == 0) {
-        magnetOn = false;
+        controller.touchEnd();
     }
 });
 
