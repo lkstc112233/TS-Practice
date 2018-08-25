e3d746c9eda17e3593dfc4fb233a3334929e86e7
Photon Cat <lkstc112233@gmail.com>
2018-08-25 16:39:06 -0400

    Fixed a spelling error

--- Spirit/Scene.ts
+++ Spirit/Scene.ts
@@ -1,28 +1,28 @@
-export interface Spirit {
+export interface Sprite {
     // Order used to determine what order to be drawn.
-    // With greater z, the later the spirit will be drawn.
-    // Therefore the less it will be overlaped by another spirit.
+    // With greater z, the later the sprite will be drawn.
+    // Therefore the less it will be overlaped by another sprite.
     readonly z: number;
     readonly decay: boolean;
-    // Returns newly added spirits.
-    generate(): Spirit[];
+    // Returns newly added sprites.
+    generate(): Sprite[];
     draw(context: CanvasRenderingContext2D): void;
 }
 
 export class Scene {
-    private spirits: Spirit[] = [];
+    private sprites: Sprite[] = [];
 
-    add(spirit:Spirit) {
-        this.spirits.push(spirit);
+    add(sprite:Sprite) {
+        this.sprites.push(sprite);
     }
 
     update() {
-        this.spirits = ([] as Spirit[]).concat(...(this.spirits.map((element) => [element].concat(element.generate()))));
-        this.spirits = this.spirits.filter((element) => !element.decay);
+        this.sprites = ([] as Sprite[]).concat(...(this.sprites.map((element) => [element].concat(element.generate()))));
+        this.sprites = this.sprites.filter((element) => !element.decay);
     }
 
     draw(context: CanvasRenderingContext2D) {
-        this.spirits = this.spirits.sort((sp1, sp2) => sp1.z - sp2.z);
-        this.spirits.map((element)=> element.draw(context));
+        this.sprites = this.sprites.sort((sp1, sp2) => sp1.z - sp2.z);
+        this.sprites.map((element)=> element.draw(context));
     }
 }
