9f57512746d333b73f33d36147f4920f1bddfafd
Photon Cat <lkstc112233@gmail.com>
2018-08-25 16:06:11 -0400

    Added sort by z

--- Spirit/Scene.ts
+++ Spirit/Scene.ts
@@ -17,6 +17,7 @@ export class Scene {
     }
 
     draw(context: CanvasRenderingContext2D) {
+        this.spirits = this.spirits.sort((sp1, sp2) => sp1.z - sp2.z);
         this.spirits = ([] as Spirit[]).concat(...(this.spirits.map((element) => {
             const added = element.draw(context);
             const result = [element];
